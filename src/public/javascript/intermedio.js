126089425f1a16283504dbb532914ea94ef474a1
pilasujg <pilasujg@gmail.com>
2023-03-15 21:44:51 +0100

    views view better

--- src/public/javascript/intermedio.js
+++ src/public/javascript/intermedio.js
@@ -1,4 +1,7 @@
-      
+     import $ from 'jquery';
+        
+
+    
       $(document).on('click', '#guardarBook', function (e) {
         let trama = $('#trama').val();
         let opinion = $('#opinion').val();
@@ -33,33 +36,49 @@
 
     });
 
-
     $(document).on('click', '#editarBook', function (e) {
-        
-        let id = $(this).attr('data-id')
-        console.log("voy a añadir miBIOGRAPHIA " + id + " ************ " + trama)
+        let trama = $('#trama').val();
+        let opinion = $('#opinion').val();
+        let titulo = $('#titulo').val();
+        let autor = $('#autor').val();
+        let genero = $('#genero').val();
+        let imagen = $('#imagen').val();
+        let id = $('#id').val();
 
-     //show in modal the book on clicked this is the way to do it
-        
+        console.log("voy a editar miBIOGRAPHIA " + autor + " ************ " + trama)
+    });
 
-        
+/*    $(document).on('click', '#guardarBook', function (e) {
+        let trama = $('#trama').val();
+        let opinion = $('#opinion').val();
+        let titulo = $('#titulo').val();
+        let autor = $('#autor').val();
+        let genero = $('#genero').val();
+        let imagen = $('#imagen').val();
 
+        console.log("voy a añadir miBIOGRAPHIA " + autor + " ************ " + trama)
 
-
- 
         $.ajax({
-                method: 'GET',
-                url: '/books/view/:id',
+                method: 'POST',
+                url: '/saveBook',
                 data: {
                     trama: trama,
                     opinion: opinion,
                     titulo: titulo,
                     autor: autor,
                     genero: genero,
-                    imagen: imagen,
-                    id: id
-                }   
+                    imagen: imagen
+                }
             })
+            .done(function (msg) {
+                showNotification(msg.message, 'success', true);
+                
+            })
+            .fail(function (msg) {
+                showNotification(msg.responseJSON.message, 'danger');
+            });
 
-             console.log("voy a añadir miBIOGRAPHIA " + id + " ************ " + trama)
-     });
+    });
+
+   */
+
